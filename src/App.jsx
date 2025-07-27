c8f86da58a26313a8c289528999bb21dff1d2ebb
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-07-27 13:28:22 +0530

    Added the crimson Pro Font

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
+import './index.css'
 import Header from './components/Header'
+import Hero1 from './components/Hero1'
+
+
+import '@fontsource/crimson-pro/400.css'; // Regular
+import '@fontsource/crimson-pro/700.css'; // Bold
 
 function App() {
 
   return (
     <>
       <Header />
+      <Hero1 />
+
     </>
   )
 }
