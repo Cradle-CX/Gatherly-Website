c5e2c7ac626583192e5ca0153150ba416adf684a
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-07-28 16:25:27 +0530

    Added the Privacy Policy Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import '@fontsource/crimson-pro/400.css'; // Regular
 import '@fontsource/crimson-pro/700.css'; // Bold
 import Renters from './pages/Renters';
 import TermsAndConditions from './pages/TermsAndConditions';
+import PrivacyPolicy from './pages/Policy';
 
 function App() {
 
   return (
     <>
-
-      <TermsAndConditions />
+      <PrivacyPolicy />
 
     </>
   )
