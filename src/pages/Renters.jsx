141a1813005918b86e74755278a914aa26671764
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-07-28 15:45:43 +0530

    Cooked the third header for renters

--- src/pages/Renters.jsx
+++ src/pages/Renters.jsx
@@ -1,6 +1,7 @@
 import Header from '../components/renters/Header.jsx';
 import Hero1 from '../components/renters/Hero1.jsx';
 import Hero2 from '../components/renters/Hero2.jsx'
+import Hero3 from '../components/renters/Hero3.jsx';
 
 const Renters = () => {
   return (
@@ -9,6 +10,7 @@ const Renters = () => {
       <Header />
       <Hero1 />
       <Hero2 />
+      <Hero3 />
 
     </>
   )
