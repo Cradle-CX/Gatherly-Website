2fde2c841be2a2e6087fbe2d04127f0a32354b6a
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-07-28 16:15:47 +0530

    Added the immediate Terms and policy page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Properties from './pages/properties';
 import '@fontsource/crimson-pro/400.css'; // Regular
 import '@fontsource/crimson-pro/700.css'; // Bold
 import Renters from './pages/Renters';
+import TermsAndConditions from './pages/TermsAndConditions';
 
 function App() {
 
   return (
     <>
 
-      <Renters />
+      <TermsAndConditions />
 
     </>
   )
